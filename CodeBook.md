9543c65ba0b29a0926f6ecef216a71af67da477a
miheyprihodko <miheyprihodko@gmail.com>
2015-05-23 08:48:16 +0300

    Data analysis (first version)

---
+++ CodeBook.md
@@ -0,0 +1,16 @@
+Code Book
+=========
+
+Our dataset contains 66 variables. Two of them - activity and subject - characterises test conditions.
+
+Other 64 variables described by its name, where:
+
+- timedomain/frequencydomain - measurements of time domain signals or frequency domain signals
+- accelerometer and gyroscope - source of signal
+- body and gravity - acceleration signal was then separated into these types of signals
+- jerk - Jerk signals received from body linear acceleration and angular velocity derived 
+- magnitude - a magnitude of three-dimensional signals calculated using the Euclidean norm
+- x, y, z - one dimension of signal
+- meanvalue, standartdeviation - type of statistic caluclated for the appropriate type of measurements
+
+Values of the dataset respresent means of means and standart deviations of 64 measurement for the appropriate activity and subject types.
--- run_analysis.R
+++ run_analysis.R
@@ -1,25 +1,6 @@
-Getting and Cleaning Data. Course project "Samsung Galaxy S Dataset_Means"
-==========================================================================
-
-In this README file we will explain all transformations of the input data-source ("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip") that let us receive the target dataset.
-
-### 0. Preparation
-
-Firstly, we should download required libraries and set general options for ouputs in this file.
-
-
-```r
 library(dplyr)
 library(knitr)
-opts_chunk$set(echo=TRUE,results="hide")
-```
 
-### 1. Merging two data sets
-
-Secondly, we should load two input data sets from three files each: values, activities and subjects.  
-
-
-```r
 col_names<-read.table("UCI HAR Dataset/features.txt",stringsAsFactors=F)
 activity_labels<-read.table("UCI HAR Dataset/activity_labels.txt",stringsAsFactors=F)
 
@@ -38,38 +19,10 @@ colnames(data_test)<-c(col_names[,2],"activity_type","subject_type")
 rownames(data_test)<-paste("test",rownames(data_test))
 
 data<-rbind(data_test,data_train)
-```
 
-### 2. Extracting the measurements on the mean and standard deviation for each measurement
-
-At this step we extract all columns from the combined data that contain "mean" or "std" in their names. The only exception - columns for weighted average of the frequency components to obtain a mean frequency (with "meanFreq" in names), because these columns present calculations on mean of easurements.
-
-
-```r
 dataset<-subset(data,select=c(grep("mean()",col_names[,2],fixed=T),grep("std()",col_names[,2],fixed=T),activity_type,subject_type))
-```
-
-### 3. Uses descriptive activity names to name the activities in the data set
-
-As we have already downloaded activity labels from the separate file, now we will replace activity type by them.
-
-
-```r
 dataset<-mutate(dataset,activity_type=activity_labels[activity_type,2])
-```
 
-### 4. Appropriately labels the data set with descriptive variable names
-
-As criterias of appropriateness for lables we can use the following:
-
-- No abbreviations
-
-- No capital letters 
-
-- Names should be self-explanatory
-
-
-```r
 labels<-colnames(dataset)
 labels<-sub("mean()","meanvalue",labels,fixed=T)
 labels<-sub("tBodyAcc","timedomain_bodyaccelerometer",labels,fixed=T)
@@ -89,22 +42,7 @@ labels<-sub("X","x",labels,fixed=T)
 labels<-sub("Y","y",labels,fixed=T)
 labels<-sub("Z","z",labels,fixed=T)
 colnames(dataset)<-labels
-```
 
-### 5. Independent tidy data set with the average of each variable for each activity and each subject
-
-As a last transformation, we will group by our dataset and calculate means of measurements for each pair of activity and subject.
-
-
-```r
 dataset_means <- summarise_each(group_by(dataset, activity_type, subject_type),funs(mean))
-```
 
-### 6. Write output into a text file
-
-Finally, we write output into a file.
-
-
-```r
 write.table(dataset_means,"dataset_means.txt",row.name=FALSE)
-```
